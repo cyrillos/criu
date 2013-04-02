2b414b8d007d814e920d51c07de0e481abcc00ea
Cyrill Gorcunov <gorcunov@openvz.org>
2013-04-02 16:26:57 +0400

    tools: cpt2 -- Add a bunch of criu file stubs
    To proceed even minimal restore we need a bunch of
    criu files to be present on disk, even if they are
    empty ones. So since this patch set aims to implement
    conversion only a few entries (tasks, regular files and
    memory) we generate stubs so crtools won't complain about
    missing files.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>

--- tools/cpt2/src/convert.c
+++ tools/cpt2/src/convert.c
@@ -8,9 +8,48 @@
 #include "protobuf.h"
 #include "context.h"
 #include "convert.h"
+#include "image.h"
 #include "read.h"
 #include "cpt2.h"
 #include "log.h"
+
+static int write_stubs(context_t *ctx)
+{
+	int fd;
+
+#define gen_stub(type)						\
+	do {							\
+		fd = open_image(ctx, CR_FD_##type, O_DUMP);	\
+		if (fd < 0)					\
+			return -1;				\
+		close(fd);					\
+	} while (0)
+
+	gen_stub(EVENTFD);
+	gen_stub(EVENTPOLL);
+	gen_stub(EVENTPOLL_TFD);
+	gen_stub(SIGNALFD);
+	gen_stub(INOTIFY);
+	gen_stub(INOTIFY_WD);
+	gen_stub(FANOTIFY);
+	gen_stub(FANOTIFY_MARK);
+	gen_stub(PIPES);
+	gen_stub(PIPES_DATA);
+	gen_stub(FIFO);
+	gen_stub(FIFO_DATA);
+	gen_stub(UNIXSK);
+	gen_stub(INETSK);
+	gen_stub(PACKETSK);
+	gen_stub(SK_QUEUES);
+	gen_stub(REMAP_FPATH);
+	gen_stub(TTY);
+	gen_stub(TTY_INFO);
+	gen_stub(RLIMIT);
+	gen_stub(NETLINKSK);
+#undef gen_stub
+
+	return 0;
+}
 
 int convert(void)
 {
@@ -42,6 +81,12 @@ int convert(void)
 	if (ret) {
 		pr_err("Failed reading dumpfile %s\n",
 		       global_opts.cpt_filename);
+		goto out;
+	}
+
+	ret = write_stubs(&ctx);
+	if (ret) {
+		pr_err("Failed writting stubs\n");
 		goto out;
 	}
 
