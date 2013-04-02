1364119aae7c6c3fe7fd607a03d77e9721395a55
Cyrill Gorcunov <gorcunov@openvz.org>
2013-04-02 19:15:59 +0400

    tools: cpt2 -- Add magicfs.h header
    I'll need these constants for file type recognition.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>

---
+++ tools/cpt2/src/include/magicfs.h
@@ -0,0 +1,12 @@
+#ifndef __CPT2_MAGICFS_H__
+#define __CPT2_MAGICFS_H__
+
+/*
+ * From kernel sources. Probably worth to share
+ * with crtools.
+ */
+#ifndef PIPEFS_MAGIC
+# define PIPEFS_MAGIC		0x50495045
+#endif
+
+#endif /* __CPT2_MAGICFS_H__ */
