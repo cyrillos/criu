f6713ae5b6b02395ca9ad34319ac86a416aa7027
Cyrill Gorcunov <gorcunov@openvz.org>
2013-04-02 14:16:18 +0400

    tools: cpt2 -- Init and fini context
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>

--- tools/cpt2/src/convert.c
+++ tools/cpt2/src/convert.c
@@ -5,12 +5,41 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#include "protobuf.h"
+#include "context.h"
 #include "convert.h"
 #include "cpt2.h"
 #include "log.h"
 
 int convert(void)
 {
+	context_t ctx;
+	int ret = -1;
+
+	context_init(&ctx);
+	pb_init();
+
+	ctx.dfd = open(global_opts.criu_dirname, O_RDONLY);
+	if (ctx.dfd < 0) {
+		pr_perror("Can't open directory %s\n",
+			  global_opts.criu_dirname);
+		goto out;
+	}
+
+	ctx.fd = open(global_opts.cpt_filename, O_RDONLY);
+	if (ctx.fd < 0) {
+		pr_perror("Can't open checkpoint file %s\n",
+			  global_opts.cpt_filename);
+		goto out;
+	}
+
+	ret = context_init_fdset_glob(&ctx);
+	if (ret)
+		goto out;
+
 	pr_err("Conversion is not yet implemented\n");
-	return -1;
+	ret = -1;
+out:
+	context_fini(&ctx);
+	return ret;
 }
